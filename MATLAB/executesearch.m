e1271402480a6f3d6f8c93a793a1ad29d5388dfc
telthien <noahsutsmo@gmail.com>
2015-02-07 20:13:42 -0800

    Refactored variables in matlab file

--- MATLAB/executesearch.m
+++ MATLAB/executesearch.m
@@ -7,8 +7,8 @@ cells=101;
 cells2=cells*cells;
 %Set cells to the amount of cells per side of probability distribution
 
-P0=(abs(peaks(cells)/(sum(sum(abs(peaks(cells)))))));
-%P0 is initial probability distribution, representing the probability
+probDistrib=(abs(peaks(cells)/(sum(sum(abs(peaks(cells)))))));
+%probDistrib is initial probability distribution, representing the probability
 %that the plane is in each cell.
 %This command populates it with the peaks data set of size
 %cells x cells and converts it to a probability distribution
@@ -24,39 +24,39 @@ searchCount=zeros(cells);
 %Initializes matrix searchCount;
 %searchCount tracks the number of times each cell has been searched
 
-alpha = 0.45;
+shipAlpha = 0.45;
 
-%Alpha is the probability of finding the plane while searching for it
+%shipAlpha is the probability of finding the plane while searching for it
 %in a particular cell with a given technology.
 
-
-shipPlacer = P0;
-maxP0valuesIni=zeros(1,ships);
-maxP0locIni=zeros(1,ships);
+initialShipDistrib = probDistrib;
+maxP0valuesInit=zeros(1,ships);
+maxP0locInit=zeros(1,ships);
 for n=1:ships
-    maxP0ini=find(shipPlacer == max(shipPlacer(:)));
-    shipPos(maxP0ini)=1;
-    searchCount(maxP0ini)=1;
-    shipPlacer(maxP0ini)=0;
-    maxP0valuesIni(n) = P0(maxP0ini);
-    maxP0locIni(n)= maxP0ini;
+    maxProbabilityPos = find(initialShipDistrib == max(initialShipDistrib(:)));
+    shipPos(maxProbabilityPos) = 1;
+    initialShipDistrib(maxProbabilityPos) = 0;
+    maxP0valuesInit(n) = probDistrib(maxProbabilityPos);
+    maxP0locInit(n) = maxProbabilityPos;
 end
+
+searchCount = searchCount .+ shipPos;
 
 %This loop places the ships in the cells with the highest probability
 %of conducting a successful search.
-%It stores the maximum initial P0 values in the vector maxP0valuesIni
-%and the original location of those values in the vector maxP0locIni
+%It stores the maximum initial probDistrib values in the vector maxP0valuesInit
+%and the original location of those values in the vector maxP0locInit
 
 epsilonMat=zeros(cells,cells);
 for n=1:cells2
-    epsilonMat(n)=(P0(n).*betaRef(n,alpha,searchCount));
+    epsilonMat(n)=(probDistrib(n).*betaRef(n,shipAlpha,searchCount));
 end
 
 %Initiates and populates the epsilon matrix epsilonMat without
 %the cost function.
 
 for n=1:cells2
-    P0(n)=P0(n).*((1/(1-(P0(n).*betaRef(n,alpha,searchCount)))));
+    probDistrib(n)=probDistrib(n).*((1/(1-(probDistrib(n).*betaRef(n,shipAlpha,searchCount)))));
 end
 
 %Applies Bayesian Search Theory for all cells using the unsearched
@@ -64,6 +64,6 @@ end
 %using the searched state for the searched cells.
 
 for n=1:ships
-    P0(maxP0locIni(n))=maxP0valuesIni(n)*((1-betaRef(maxP0locIni(n),alpha,searchCount))/...
-        (1-P0(n).*betaRef(n,alpha,searchCount)));
+    probDistrib(maxP0locInit(n))=maxP0valuesInit(n)*((1-betaRef(maxP0locInit(n),shipAlpha,searchCount))/...
+        (1-probDistrib(n).*betaRef(n,shipAlpha,searchCount)));
 end
