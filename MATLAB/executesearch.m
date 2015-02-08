4cc626fe991c0c2fcf9a674a6e16589336342a5c
magus35 <joemarsili@me.com>
2015-02-07 21:54:19 -0800

    Added ship movements

--- MATLAB/executesearch.m
+++ MATLAB/executesearch.m
@@ -3,7 +3,8 @@
 %  
 %  flightpath
 
-cells = 150;
+searchIterations = 5;
+cells = 15;
 cells2 = cells * cells;
 %Set cells to the amount of cells per side of probability distribution
 
@@ -14,8 +15,8 @@ probDistrib = (abs(peaks(cells) / (sum(sum(abs(peaks(cells)))))));
 %cells x cells and converts it to a probability distribution
 %(sum of all elements = 1)
 
-ships=56;
-shipPos=zeros(cells);
+ships=12;
+shipLocMatrix=zeros(cells);
 %ships = # of ships searching
 %Initiliazes matrix shipPos;
 %shipPos tracks the position of each ship
@@ -33,7 +34,7 @@ initialShipDistrib = probDistrib;
 shipLocations=zeros(1,ships);
 for n=1:ships
     maxProbabilityPos = find(initialShipDistrib == max(initialShipDistrib(:)));
-    shipPos(maxProbabilityPos) = 1;
+    shipLocMatrix(maxProbabilityPos) = 1;
     initialShipDistrib(maxProbabilityPos) = 0;
     shipLocations(n) = maxProbabilityPos;
 end
@@ -43,17 +44,44 @@ end
 %It stores the maximum initial probDistrib values in the vector maxP0valuesInit
 %and the original location of those values in the vector maxP0locInit
 
-betaMat = shipAlpha * ((1 - shipAlpha) .^ searchCount);
-betaMat;
+%First iteration under assumption of failure
 
-epsilonMatNoCost = probDistrib .* betaMat;
-
-% Then, assume searching has failed.
-
-for n=1:ships
-    shipLoc = shipLocations(n);
-    tmpProbDist = probDistrib .* (1 / (1 - betaMat(shipLoc)*probDistrib(shipLoc)));
-    tmpProbDist(shipLoc) = probDistrib(shipLoc) * ((1 - betaMat(shipLoc))/(1-probDistrib(shipLoc)*betaMat(shipLoc)));
-    probDistrib = tmpProbDist;
+for searchNum=1:8
+    for n=1:ships
+        shipLoc = shipLocations(n);
+        tmpProbDist = probDistrib .* (1 / (1 - betaMat(shipLoc)*probDistrib(shipLoc)));
+        tmpProbDist(shipLoc) = probDistrib(shipLoc) * ((1 - betaMat(shipLoc))/(1-probDistrib(shipLoc)*betaMat(shipLoc)));
+        probDistrib = tmpProbDist;
+    end
+    
+    searchCount = searchCount + shipLocMatrix;
+    betaMat = shipAlpha * ((1 - shipAlpha) .^ searchCount);
+    
+    newShipLocations = zeros(1,ships);
+    newShipLocMatrix = zeros(cells,cells);
+    for n=1:ships
+        distanceMatrix = zeros(cells, cells);
+        shipRow = mod(shipLocations(n)-1, cells)+1; % Gets row based on single-dimension coord
+        shipCol = (shipLocations(n)-shipRow)/cells+1; % Gets col based on single-dimension coord
+        for epRow=1:cells
+            for epCol=1:cells
+                distanceMatrix(epRow, epCol) = sqrt((shipRow - epRow)^2+(shipCol - epCol)^2);
+                if distanceMatrix(epRow, epCol) == 0
+                    distanceMatrix(epRow, epCol) = Inf;
+                end
+            end
+        end
+        
+        perShipEpsilon = (probDistrib ./ distanceMatrix);
+        perShipEpsilon = betaMat .* perShipEpsilon;
+        perShipEpsilon = (1 - newShipLocMatrix) .* perShipEpsilon;
+        
+        maxProbabilityPos = find(perShipEpsilon == max(perShipEpsilon(:)));
+        newShipLocMatrix(maxProbabilityPos) = 1;
+        newShipLocations(n) = maxProbabilityPos;
+        %figure(n); mesh(newShipLocMatrix);
+    end
+    
+    shipLocations = newShipLocations;
+    shipLocMatrix = newShipLocMatrix;
 end
-
