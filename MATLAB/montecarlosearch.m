a2d3ef01c2653e8832087db59181dd795c21e986
magus35 <joemarsili@me.com>
2015-02-08 02:04:20 -0800

    Monte Carlo multithread search added - functional

---
+++ MATLAB/montecarlosearch.m
@@ -0,0 +1,59 @@
+numTrials = 100;
+
+%Input ranges
+searchItersMin = 10;
+searchItersMax = 100;
+alphaMin = 0.25;
+alphaMax = 0.5;
+shipCountMin = 5;
+shipCountMax = 50;
+
+%Computed ranges
+searchItersRng = searchItersMax - searchItersMin;
+alphaRng = alphaMax - alphaMin;
+shipCountRng = shipCountMax - shipCountMin;
+
+%Input collection
+m_searchIters = zeros(1,numTrials);
+m_alpha = zeros(1,numTrials);
+m_shipCount = zeros(1,numTrials);
+
+%Output collection
+m_percentSuccess = zeros(1,numTrials);
+m_distanceTraveled = zeros(1,numTrials);
+m_numSearchedSquares = zeros(1,numTrials);
+
+%Generate data
+
+matlabpool open
+
+disp('Generating data...')
+t0 = clock;
+parfor i=1:numTrials
+    randSearchIters = round(rand*searchItersRng) + searchItersMin;
+    randAlpha = (round(rand*alphaRng*1000)/1000)+alphaMin;
+    randShipCount = round(rand*shipCountRng)+shipCountMin;
+
+    m_searchIters(i) = randSearchIters;
+    m_alpha(i) = randAlpha;
+    m_shipCount(i) = randShipCount;
+end
+disp(num2str(round(etime(clock,t0)*1000)));
+
+disp('Running simulation...')
+t0 = clock;
+parfor i=1:numTrials
+    randSearchIters = m_searchIters(i);
+    randAlpha = m_alpha(i);
+    randShipCount = m_shipCount(i);
+    %disp(strcat(num2str(i),'|',num2str(randSearchIters),'|',num2str(randAlpha),'|',num2str(randShipCount)));
+    
+    [pr1, pr2, pr3] = executesearchfn(randSearchIters, randShipCount, randAlpha);
+    
+    m_percentSuccess(i) = pr1;
+    m_distanceTraveled(i) = pr2;
+    m_numSearchedSquares(i) = pr3;
+end
+disp(num2str(round(etime(clock,t0)*1000)));
+
+matlabpool close
--- MATLAB/randomizerow.m
+++
@@ -1,8 +0,0 @@
-function r=randomizerow(columns,min,max)
-%columns = #columns
-%min = minimum value of output
-%max = maximum value of output
-    r=(max-min).*rand(1,columns) + min;
-
-end
-
