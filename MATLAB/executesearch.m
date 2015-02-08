a58a3565d536f454cfc986e5860ee2da4089b457
magus35 <joemarsili@me.com>
2015-02-07 20:40:51 -0800

    made mroe bettteererer
    I am good at commit logs.

--- MATLAB/executesearch.m
+++ MATLAB/executesearch.m
@@ -3,18 +3,18 @@
 %  
 %  flightpath
 
-cells=101;
-cells2=cells*cells;
+cells = 150;
+cells2 = cells * cells;
 %Set cells to the amount of cells per side of probability distribution
 
-probDistrib=(abs(peaks(cells)/(sum(sum(abs(peaks(cells)))))));
+probDistrib = (abs(peaks(cells) / (sum(sum(abs(peaks(cells)))))));
 %probDistrib is initial probability distribution, representing the probability
 %that the plane is in each cell.
 %This command populates it with the peaks data set of size
 %cells x cells and converts it to a probability distribution
 %(sum of all elements = 1)
 
-ships=3;
+ships=56;
 shipPos=zeros(cells);
 %ships = # of ships searching
 %Initiliazes matrix shipPos;
@@ -30,40 +30,30 @@ shipAlpha = 0.45;
 %in a particular cell with a given technology.
 
 initialShipDistrib = probDistrib;
-maxP0valuesInit=zeros(1,ships);
-maxP0locInit=zeros(1,ships);
+shipLocations=zeros(1,ships);
 for n=1:ships
     maxProbabilityPos = find(initialShipDistrib == max(initialShipDistrib(:)));
     shipPos(maxProbabilityPos) = 1;
     initialShipDistrib(maxProbabilityPos) = 0;
-    maxP0valuesInit(n) = probDistrib(maxProbabilityPos);
-    maxP0locInit(n) = maxProbabilityPos;
+    shipLocations(n) = maxProbabilityPos;
 end
-
-searchCount = searchCount .+ shipPos;
 
 %This loop places the ships in the cells with the highest probability
 %of conducting a successful search.
 %It stores the maximum initial probDistrib values in the vector maxP0valuesInit
 %and the original location of those values in the vector maxP0locInit
 
-epsilonMat=zeros(cells,cells);
-for n=1:cells2
-    epsilonMat(n)=(probDistrib(n).*betaRef(n,shipAlpha,searchCount));
-end
+betaMat = shipAlpha * ((1 - shipAlpha) .^ searchCount);
+betaMat;
 
-%Initiates and populates the epsilon matrix epsilonMat without
-%the cost function.
+epsilonMatNoCost = probDistrib .* betaMat;
 
-for n=1:cells2
-    probDistrib(n)=probDistrib(n).*((1/(1-(probDistrib(n).*betaRef(n,shipAlpha,searchCount)))));
-end
-
-%Applies Bayesian Search Theory for all cells using the unsearched
-%state. The loop below applies the Bayesian Search Theory for all cells
-%using the searched state for the searched cells.
+% Then, assume searching has failed.
 
 for n=1:ships
-    probDistrib(maxP0locInit(n))=maxP0valuesInit(n)*((1-betaRef(maxP0locInit(n),shipAlpha,searchCount))/...
-        (1-probDistrib(n).*betaRef(n,shipAlpha,searchCount)));
+    shipLoc = shipLocations(n);
+    tmpProbDist = probDistrib .* (1 / (1 - betaMat(shipLoc)*probDistrib(shipLoc)));
+    tmpProbDist(shipLoc) = probDistrib(shipLoc) * ((1 - betaMat(shipLoc))/(1-probDistrib(shipLoc)*betaMat(shipLoc)));
+    probDistrib = tmpProbDist;
 end
+
