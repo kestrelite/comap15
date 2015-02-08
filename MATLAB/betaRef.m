ae457183c2d7c3ea619f812108e4d199c3eaae33
magus35 <joemarsili@me.com>
2015-02-07 19:18:36 -0800

    Changed betaRef, documented changes

--- MATLAB/betaRef.m
+++ MATLAB/betaRef.m
@@ -1,6 +1,12 @@
 function a=betaRef(j,alpha,searchCount)
-% Calculates the beta value of the j'th cell by reference
+% Calculates the beta value of the j'th cell by referencing
 % the matrix searchCount with respect to the success rate
 % of searching by means of a given technology alpha.
-    a=alpha.*(1-alpha).^(searchCount(j)-1);
+%% NOTE: This is using the formula:
+%% alpha*(1-alpha)^k-1, with k-1 replaced with k.
+%% The implementation of this function, primarily in populating
+%% the epsilonMat matrix, requires that this substitution be made
+%% in order to accurately report the search effectiveness (epsilon)
+%% for the next search in any given cell.
+    a=alpha.*(1-alpha).^(searchCount(j));
 end
